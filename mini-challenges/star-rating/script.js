ebfedc4d4f5979eb548f3fe5bc3c11c56e8f058e
krishna-saini <krishnapraanjal10@gmail.com>
2022-12-30 07:41:16 +0530

    hover functionality add

--- mini-challenges/star-rating/script.js
+++ mini-challenges/star-rating/script.js
@@ -2,30 +2,36 @@ const starContainer = document.querySelector(".star-container");
 const star = document.querySelectorAll(".star");
 
 // filling the stars on hovering
-// starContainer.addEventListener("mouseover", (e) => {
-//   if (e.target.className === "star star-empty") {
-//     const targetIndex = e.target.getAttribute("data-index");
-//     // fill those whose data index <= hovered data-index
-//     // means add star-filled class
-//     [...star].map((el) => {
-//       if (el.getAttribute("data-index") <= targetIndex) {
-//         el.classList.add("star-filled");
-//         el.classList.remove("start-empty");
-//       }
-//     });
-//   }
-// });
+starContainer.addEventListener("mouseover", (e) => {
+  console.log("mouseover", e.target);
+  // if (e.target.className === "star star-empty") {
+    const targetIndex = e.target.getAttribute("data-index");
+    // fill those whose data index <= hovered data-index
+    // means add star-filled class
+    star.forEach((el) => {
+      if (el.getAttribute("data-index") <= targetIndex) {
+        el.classList.add("star-filled");
+        el.classList.remove("start-empty");
+      }else{
+        el.classList.remove("star-filled")
+      }
+    });
+  // }
+});
 
 // filled them permanently if star is clicked
 starContainer.addEventListener("click", (e) => {
   // find data index of star where it is clicked
   const clickedStarIndex = e.target.getAttribute("data-index");
+  console.log(clickedStarIndex);
 
   // jha tk hover kiya h wo star  filled krdo
-  [...star].map((el) => {
-    if (el.getAttribute("data-index") <= targetIndex) {
+  star.forEach((el) => {
+    if (el.getAttribute("data-index") <= clickedStarIndex) {
+      console.log("yes");
       el.classList.add("star-filled");
     } else {
+      console.log("no");
       el.classList.remove("start-filled");
     }
   });
