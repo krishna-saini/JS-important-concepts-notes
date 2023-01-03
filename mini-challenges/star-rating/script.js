5a9803d607a10d63dc96d2b96b30b3c885772ce3
krishna-saini <krishnapraanjal10@gmail.com>
2023-01-03 07:50:51 +0530

    star rating system done

--- mini-challenges/star-rating/script.js
+++ mini-challenges/star-rating/script.js
@@ -1,45 +1,54 @@
 const starContainer = document.querySelector(".star-container");
-const star = document.querySelectorAll(".star");
+const stars = document.querySelectorAll(".stars");
+let clickedStarIndex;
+let hoveredIndex;
 
 // filling the stars on hovering
 starContainer.addEventListener("mouseover", (e) => {
-  console.log("mouseover", e.target);
-  // if (e.target.className === "star star-empty") {
-    const targetIndex = e.target.getAttribute("data-index");
-    // fill those whose data index <= hovered data-index
-    // means add star-filled class
-    star.forEach((el) => {
-      if (el.getAttribute("data-index") <= targetIndex) {
-        el.classList.add("star-filled");
-        el.classList.remove("start-empty");
-      }else{
-        el.classList.remove("star-filled")
-      }
-    });
-  // }
+  // console.log("mousehover");
+  hoveredIndex = e.target.getAttribute("data-index");
+  console.log(hoveredIndex);
+  // fill those whose data index <= hovered data-index
+  // means add star-filled class
+  stars.forEach((el) => {
+    if (el.getAttribute("data-index") <= hoveredIndex) {
+      el.classList.add("star-filled");
+      el.classList.remove("star-empty");
+    } else {
+      el.classList.remove("star-filled");
+      el.classList.add("star-empty");
+    }
+  });
 });
 
 // filled them permanently if star is clicked
 starContainer.addEventListener("click", (e) => {
   // find data index of star where it is clicked
-  const clickedStarIndex = e.target.getAttribute("data-index");
+  clickedStarIndex = e.target.getAttribute("data-index");
   console.log(clickedStarIndex);
 
-  // jha tk hover kiya h wo star  filled krdo
-  star.forEach((el) => {
+  // jha tk hover kiya h wo star IS ALREADY FILLED
+  stars.forEach((el) => {
     if (el.getAttribute("data-index") <= clickedStarIndex) {
-      console.log("yes");
       el.classList.add("star-filled");
+      el.classList.remove("star-empty");
     } else {
-      console.log("no");
-      el.classList.remove("start-filled");
+      el.classList.remove("star-filled");
+      el.classList.add("star-empty");
     }
   });
 });
 
-// // empty the stars on mouseout
-// starContainer.addEventListener("mouseleave", () => {
-//   [...star].map((el) => {
-//     el.classList.remove("star-filled");
-//   });
-// });
+// / PREVENT DESELECTION OF STARS ONCE USER MOVES AWAY
+starContainer.addEventListener("mouseleave", () => {
+  stars.forEach((el) => {
+    if (el.getAttribute("data-index") <= clickedStarIndex) {
+      console.log("here2");
+      el.classList.add("star-filled");
+      el.classList.remove("star-empty");
+    } else {
+      el.classList.remove("star-filled");
+      el.classList.add("star-empty");
+    }
+  });
+});
